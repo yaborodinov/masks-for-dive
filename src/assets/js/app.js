b6bf19a691c64fdd4af8d60325ed95e05b630cc9
Alexandr <yaborodinov@gmail.com>
2021-07-22 11:20:25 +0300

    перемещаю проект с ноута

---
+++ src/assets/js/app.js
@@ -0,0 +1,70 @@
+
+// slider ============
+
+
+
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+    slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    
+  ]
+});
+
+$('.reviews-slider').slick({
+  infinite: true,
+  slidesToShow: 2,
+    slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    
+  ]
+});
+
+
